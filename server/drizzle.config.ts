65e145ef4067e8c7e119166268aefab2d8087656
Bisheshops <bisheshshr@gmail.com>
2026-08-24 05:17:41 +0000

    chore: add .dockerignore file to exclude unnecessary files from Docker context; update schema path in drizzle.config.ts

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
 export default defineConfig({
-  schema: "./src/models/PaymentModel.ts",
+  schema: "./models/PaymentModel.ts",
   
   out: "./drizzle",
   
